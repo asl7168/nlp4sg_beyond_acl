5bc816508597f08a44ffc19abf85a3e5aaef172a
asl7168 <adamleif@g.ucla.edu>
2025-09-18 17:16:31 -0700

    prepared to test code locally

--- paths.py
+++ paths.py
@@ -1,9 +1,9 @@
-# NOTE: while some functions provide the option to provide custom filepaths, we recommend only
-# modifying the paths in this file (namely corpora_path, since all others paths rely on it). 
-# Custom filepaths are untested
+# NOTE: while some functions provide (or previously provided) the option to provide custom 
+# filepaths, we recommend only modifying the paths in this file (namely corpora_path, since 
+# all others paths rely on it). Otherwise, custom filepaths are untested
 
 # main directory, in which datasets are downloaded and subcorpora are built
-corpora_path = "/projects/b1170/corpora/comp_ling_meta" 
+corpora_path = "/projects/b1170/corpora/comp_ling_meta"
 
 datasets_path = f"{corpora_path}/datasets"  # where datasets are stored
 authors_path = f"{corpora_path}/authors"  # ---
